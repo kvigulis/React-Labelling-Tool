e54a6fb43fb4717f44349999578bb68334cfe998
Karlis Vigulis <karlis.vigulis@city.ac.uk>
2020-11-01 21:00:21 +0000

    initial

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Router, Route} from 'react-router-dom';
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="ui container">
+        App
+      </div>
+    )
+  }
+}
+
+export default App;
